1d3949726fc26229fda0d5b032c3359e8a3896c9
Vyacheslav Zhadeyev <vzhadeyev@gmail.com>
2018-01-08 19:39:51 +0600

    Lab 1

--- Labs/Lab1/lab1.py
+++ Labs/Lab1/lab1.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def load_reviews_data(reviews_data_path):
     """Loads the reviews dataset as a list of dictionaries.
 
@@ -14,7 +15,13 @@ def load_reviews_data(reviews_data_path):
         A list of dictionaries where each dictionary maps column name
         to value for a row in the reviews dataset.
     """
-    raise NotImplementedError
+    data = []
+    with open(reviews_data_path, encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t')
+        for row in reader:
+            data.append(row)
+    return data
+
 
 def load_toy_data(toy_data_path):
     """Loads the 2D toy dataset as numpy arrays.
@@ -26,7 +33,11 @@ def load_toy_data(toy_data_path):
         A tuple (features, labels) in which features is an Nx2 numpy
         matrix and labels is a length-N vector of +1/-1 labels.
     """
-    raise NotImplementedError
+    data = np.loadtxt(toy_data_path)
+    X = data[:,1:]
+    y = data[:,0]
+    return X, y
+
 
 def plot_toy_data(data, labels):
     """Plots the toy data in 2D.
@@ -35,4 +46,12 @@ def plot_toy_data(data, labels):
         data(ndarray): An Nx2 ndarray of points.
         labels(ndarray): A length-N vector of +1/-1 labels.
     """
-    raise NotImplementedError
+    data1 = data[labels==-1]
+    data2 = data[labels==1]
+    plt.scatter(data1[:, 0], data1[:, 1], color='red')
+    plt.scatter(data2[:, 0], data2[:, 1], color='blue')
+    x = [3.2, -2]
+    y = [-2, 4]
+    plt.plot(x, y)
+    plt.show()
+    return
--- Labs/Lab1/main.py
+++ Labs/Lab1/main.py
@@ -7,12 +7,13 @@ import lab1
 
 def check_reviews_data(reviews_data):
     assert len(reviews_data) == 2000
+    print(reviews_data[0].keys())
     assert sorted(reviews_data[0].keys()) == sorted(['helpfulN', 'helpfulY', 'productId', 'sentiment', 'summary', 'text', 'userId'])
 
 def check_toy_data(toy_data, toy_labels):
     assert toy_data.shape == (200, 2)
     assert toy_labels.shape == (200,)
-    assert np.array_equal(np.sum(toy_data, axis=0), np.array([ 185.6102,  202.9751]))
+    assert np.allclose(np.sum(toy_data, axis=0), np.array([185.6102,  202.9751]))
     assert set(toy_labels) == {-1.0, 1.0}
 
 if __name__ == '__main__':
